fdb186cbe867b2b0f5e31a81941be85d5dcc9dca
Brayan <brayancorb@gmail.com>
2016-04-29 20:16:24 -0500

    Update Edit1.java

--- Edit1.java
+++ Edit1.java
@@ -1,25 +1,26 @@
+package proyecto2;
+import java.util.StringTokenizer;
 import java.util.Scanner; 
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Proyecto2 {
     public static void main (String [ ] args){
+        StringTokenizer tokens;
         boolean valorboolean = true;
         String postal ="";
-        System.out.println("Bienvenido");
+        String cadena;
+        
+        System.out.println("Bienvenido al Servicio Postal el Pony Mensajero");
         System.out.print ("Introduzca su còdigo postal de 5 dígitos: ");
         Scanner input = new Scanner(System.in);
         postal = input.nextLine (); 
-        //con ayuda del metodo .lenght verifica que sean 5 numeros
+
         if (postal.length() != 5) {
-            System.out.println ("Error ingresaste mal tu còdigo");
-        } else { System.out.println ("Bien");}
-        //con ayuda de este for recorre el codigo y ve si son numeros
+            System.out.println ("Direcciòn invàlida");
+        } else { System.out.println ("Direcciòn vàlida");}
+
         for (int i=0;i< postal.length(); i++) {
-            System.out.print (Verificando que sean numeros: " + postal.charAt(i));  
+            System.out.print ("Verificando que los nùmeros sean dìgotos: " + postal.charAt(i));  
             if (Character.isDigit(postal.charAt(i)) == false) {
                 System.out.println ("  No es un número, entrada no válida");
                 valorboolean = false;
@@ -30,25 +31,25 @@ public class Proyecto2 {
  
         }
         else {System.out.println ("Hay caracteres no numéricos"); }
-               
          try{
             // Abrimos el archivo
-            FileInputStream fistream = new FileInputStream("C:/Users/Miranda/Downloads/CPdescarga.txt");//cambia la direccion del archivo
-            // Creamos el objeto de entrada
-            DataInputStream entrada = new DataInputStream(fistream);
+            FileReader Fire = new FileReader("C:/Users/Miranda/Downloads/CPdescarga.txt");
             // Creamos el Buffer de Lectura
-            BufferedReader buffer = new BufferedReader(new InputStreamReader(entrada));
-            String strLinea;
+            BufferedReader buffer = new BufferedReader(Fire);
+            // Comienza lectura de lineas
             // Leer el archivo linea por linea
-            while ((strLinea = buffer.readLine()) != null)   {
-                // Imprimimos la línea por pantalla
-                System.out.println (strLinea);
+            while ((cadena = buffer.readLine())!= null)   {
+                String datobuscado = cadena;
+                //compara lo ingresado con lo leido
+                if (datobuscado.startsWith(postal)){
+                    //imprime lo comparado
+                    System.out.println("Su direcciòn es: "+datobuscado);
+                }
             }
-            // Cerramos el archivo
-            entrada.close();
         }catch (Exception e){ //Catch de excepciones
             System.err.println("Ocurrio un error: " + e.getMessage());
         }
+
     }
 }
     
